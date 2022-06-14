f94d2ed2104ece3591fd8ff880dc346dda7a61ff
gwchoi <gwchoi@altnm.co.kr>
2022-06-14 22:36:46 +0900

    JavaStudy01 first init

---
+++ src/main/java/com/kwchoi/javastudy01/JavaStudy01Application.java
@@ -0,0 +1,13 @@
+package com.kwchoi.javastudy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudy01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStudy01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwchoi/javastudy01/JavaStudy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwchoi.javastudy01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudy01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
